7918b54c118fbbd1047c668e4ee2fc968f82453a
Walace da Silva <walacespo@gmail.com>
2022-09-17 11:31:41 -0300

    Added component header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello World!!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
