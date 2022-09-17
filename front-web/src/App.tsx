30c6f4bf033ee31e2ebb69b42114e41d0abfdf68
Walace da Silva <walacespo@gmail.com>
2022-09-17 12:02:09 -0300

    Creating home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
